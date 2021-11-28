3cb2c9e968baf4f994185b3eb771a7f82f90a1a3
minknhom <19521844@gm.uit.edu.vn>
2021-11-28 23:35:32 +0700

    test

---
+++ CanHo/Models/CanHoModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CanHo.Models
+{
+    public class CanHoModel
+    {
+        private string maCH;
+        private string tenCH;
+
+        public CanHoModel()
+        {
+        }
+
+        public CanHoModel(string maCH, string tenCH)
+        {
+            this.MaCH = maCH;
+            this.TenCH = tenCH;
+        }
+
+        public string MaCH { get => maCH; set => maCH = value; }
+        public string TenCH { get => tenCH; set => tenCH = value; }
+    }
+}
---
+++ CanHo/Models/NhanVienModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CanHo.Models
+{
+    public class NhanVienModel
+    {
+        private string maNV;
+        private string tenNV;
+        private string soDT;
+        private string gTinh;
+
+        public NhanVienModel()
+        {
+        }
+
+        public NhanVienModel(string maNV, string tenNV, string soDT, string gTinh)
+        {
+            this.MaNV = maNV;
+            this.TenNV = tenNV;
+            this.SoDT = soDT;
+            this.GTinh = gTinh;
+        }
+
+        public string MaNV { get => maNV; set => maNV = value; }
+        public string TenNV { get => tenNV; set => tenNV = value; }
+        public string SoDT { get => soDT; set => soDT = value; }
+        public string GTinh { get => gTinh; set => gTinh = value; }
+
+        public int SoLan { get; set; }
+    }
+}
